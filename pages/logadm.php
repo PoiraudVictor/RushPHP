ffc7418f2d697918be132428e24859cf1ce7b808
amarc27 <antoinefl.marc@gmail.com>
2019-12-15 19:51:14 +0100

    deletd

--- pages/logadm.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if ($_POST["login"] === "admin" && $_POST["passwd"] === "tonton" && $_POST["submit"] && $_POST["submit"] === "OK") {
-    header('Location: admin.html');
-}
-else
-    //integration Front 
-?>
